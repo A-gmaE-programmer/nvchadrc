8860a19a75525eed1ecf5f447389e68d6daf7949
A-gamE-programmer <kevinclangner@gmail.com>
2024-07-06 19:12:45 +0800

    Finally fix todo comments

--- lua/plugins/folke.lua
+++ lua/plugins/folke.lua
@@ -7,7 +7,31 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = false,
-    ops = {}
+    cmd = { "TodoTrouble", "TodoTelescope", "TodoLocList", "TodoQuickFix" },
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    opts = {
+      highlight = {
+        before = "fg",
+        keyword = "bg",
+        pattern = [[<(KEYWORDS)\s*]],
+      }
+    },
+    config = function (_, opts)
+      require("todo-comments").setup(opts)
+    end,
   },
 }
+
+-- PERF: asdfad
+-- test
+-- HACK: asfdadf
+-- test
+-- TODO: asdfadf
+-- test
+-- NOTE: dsaf
+-- test
+-- FIX: asdfads
+-- test
+-- WARNING: asfdadf
+-- test
+
